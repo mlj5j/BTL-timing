f78111e7eb9fec66951c7e16f03dbadd0cb3ff14
Matt Joyce <mlj5j@virginia.edu>
2017-09-01 18:00:23 +0200

    Added time difference plot and added digitizer reader

--- macros/synchplot.C
+++ macros/synchplot.C
@@ -17,9 +17,6 @@
 
 void synchplot()
 {
-
-//    int k = 1;  //counter for debugging
-//    int l = 1;  //counter for debugging
     
     int tpet_events, nino_events;
     long long tpetshift = 0;
@@ -28,6 +25,13 @@ void synchplot()
     bool valid = true;
     double scaledninotime;
     int board;
+    int nDigiSamples = 18430;
+    double time_dif;
+    double prev_tpettime = 0;
+    double prev_ninotime = 0;
+    double tpet_cycles;
+    double nino_cycles;
+
 
     //Declaring variables we get from the root files
     long long tpettime;
@@ -36,15 +40,25 @@ void synchplot()
     double scaledtpettime;
     unsigned long long ninotime[2];
     unsigned int timeboard[2];
+    unsigned int digiChannel[nDigiSamples];
+    unsigned int digiGroup[nDigiSamples];
+    float digiSampleValue[nDigiSamples];
+    float x;
+    float y;
+    float energy;
     
 
     //initializing the TGraph we'll use to plot the two times
     TGraph *g1 = new TGraph();
-//    TGraph *g2 = new TGraph();
+    TGraph *g2 = new TGraph();
+//    TGraph *g3 = new TGraph();
+
+//########################Loading the TOFPET file into ROOT############################################
 
 //    TFile *f_tpet = new TFile("TOFPET/R_2017_06_30__13_54_21_c.root");
     TFile *f_tpet = new TFile("TOFPET/R_2017_06_30__13_54_21_s.root");
 //    TFile *f_tpet = new TFile("TOFPET/R_2017_06_30__13_54_21_c_trigger.root"); //loading the tofpet root file
+
     if (f_tpet == 0)
     {
 	cout << "Error:  Cannot find TOFPET file!" << endl;
@@ -53,30 +67,26 @@ void synchplot()
     else
 	cout << "File found and loaded!" << endl;
 
+//###############################################################################################
 
-/*######################Commented to test TChain##########################################################
-    TFile *f_nino = new TFile("/afs/cern.ch/user/j/jihan/work/public/nino_8076.root");
-//  TFile *f_nino = new TFile("/afs/cern.ch/work/p/pbarria/public/BTL_Utility/NINO/8076/NINO_8076.root");
-//    TFile *f_nino = new TFile("NINO/8076/1.root"); //loading the h4daq root file
-    if (f_nino == 0)
-    {
-	cout << "Error:  Cannot find NINO file!" << endl;
-	return;
-    }
-    else
-	cout << "File found and loaded!" << endl;
-*///######################################################################################################
+
+
+//######Making TChain of the H4DAQ files once the spills have been combined for each run##########
 
     TChain *ninotree = new TChain("H4tree");
     ninotree->Add("TOFPET_data/Run_8076.root");
     ninotree->Add("TOFPET_data/Run_8077.root");
     ninotree->Add("TOFPET_data/Run_8078.root");
 
+//#############################################################################################
 
     TTree* tpettree = (TTree*) f_tpet->Get("data");
 
     tpettree->SetBranchAddress("step1",&step1);
     tpettree->SetBranchAddress("step2",&step2);
+    tpettree->SetBranchAddress("x",&x);
+    tpettree->SetBranchAddress("y",&y);
+    tpettree->SetBranchAddress("energy",&energy);
 
 //    tpettree->SetBranchAddress("time2",&tpettime);
 //    tpettree->SetBranchAddress("channelID2",&chID);
@@ -86,32 +96,33 @@ void synchplot()
 
     ninotree->SetBranchAddress("evtTime",&ninotime);
     ninotree->SetBranchAddress("evtTimeBoard",&timeboard);
+    ninotree->SetBranchAddress("digiChannel",&digiChannel);
+    ninotree->SetBranchAddress("digiGroup",&digiGroup);
+    ninotree->SetBranchAddress("digiSampleValue",&digiSampleValue);
 
 
     tpet_events = tpettree->GetEntries();
     nino_events = ninotree->GetEntries();
     cout << "tpet_events = " << tpet_events << endl;
 
-
-    int counter =0;
     //Looping through the events in the TOFPET root file.  
-    for (int i=0; i<1e5 /*tpet_events*/; i++)
+    for (int i=0; i<tpet_events; i++)
     {
 	if (valid == false) //This breaks the loop if we run out of NINO events
 	    break;
 	tpettree->GetEntry(i);
+	if (i%10000 == 0)
+	    cout << "At event " << i << endl;
 
 	if (step1 == 4)  //Choosing only events from TOFPET having 4V threshold
 	{
+//	    cout << "step1 == 4" << endl;
 //	    if (step2 == 32)
 //	    {
 		if (chID == 896)  //Choosing only triggers coming from ASIC 2
 		{
-//		    g2->SetPoint(i,(double)i,(double)step1);
-//		    counter++;
-//		    cout << "event " << counter << endl;
 		    ninotree->GetEntry(nino_counter);  //Getting event from NINO corresponding to ASIC 2 trigger
-		    
+		      
 		    if (timeboard[0] == 16842753)
 			board = 0;
 		    else 
@@ -127,32 +138,44 @@ void synchplot()
 		    ninotime[board] -= ninoshift;  //Applying the shift needed to zero the first event time
 		    scaledtpettime = (double)tpettime;  //Converting the topfet time from ps to ns
 		    scaledninotime = (double)ninotime[board]*1e6;  //Converting from UNIX time (ms) to ps
-//		    cout << "nino_counter = " << nino_counter << endl;
-		    g1->SetPoint(nino_counter,scaledninotime,scaledtpettime); 
-/*
-//----------------------For debugging---------------------------------------------
-		    if (scaledtpettime > 4e10 && scaledtpettime < 8e10)
-		    {
-			cout << "time step 1 = " << scaledtpettime << endl;
-//			cout << "number of step events = " << k << endl;
-			k++;
-		    }
-		    if (scaledtpettime > 8e10 && scaledtpettime < 1e11)
-		    {
-			cout << "time step 2 = " << scaledtpettime << endl;
-//			cout << "number of second step events = " << l << endl;
-			l++;
-		    }
-//-------------------------------------------------------------------------------
-*/
-		    //----------------For debugging----------------------------- 
-		    /*
-		    if (nino_counter < 610)
-			cout << scaledninotime << endl;
-//		    cout << nino_events << endl;
-//		    ------------------------------------------------------------
-		    */
+		    time_dif = abs(scaledtpettime - scaledninotime);
 
+//#######################################Time cuts###############################################
+
+//This if statement only accepts events within a certain time range, but it's still a work in progress so it's disabled for now
+	//	    if (abs(time_dif)<1e5)
+	//	    {
+			g1->SetPoint(nino_counter,scaledninotime,scaledtpettime);
+			g2->SetPoint(nino_counter,scaledtpettime,time_dif);
+	//	    }
+	
+	//	    else 
+	//	    {
+	//		nino_counter++;
+	//		i -= 1;
+	//		continue;
+	//	    }
+		    
+		    //g2->SetPoint(nino_counter,(double)nino_counter,time_dif);
+//		    g3->SetPoint(nino_counter,x,energy);
+//		    if (scaledtpettime > 5e13)
+//			break; 
+//####################################################################################################
+/*
+##################Reading Digi Channels################################################################
+		    for (int k=0; k<nDigiSamples; k++)
+		    {
+			if (digiGroup[k] == 0)
+			{
+			    if (digiChannel[k] == 7)
+			    {
+				g2->SetPoint(nino_counter, scaledtpettime, digiSampleValue[k]);
+				
+			    }
+			}
+		    }
+#######################################################################################################
+*/
 		    nino_counter++;
 		    if (nino_counter+1 == nino_events) //This checks that we don't try to read events that aren't there.
 		    {
@@ -161,6 +184,10 @@ void synchplot()
 		    }
 		    if (i+1 == tpet_events)
 			cout << "Ran out of TOFPET events.  Number of events = " << nino_counter << endl;
+		    nino_cycles = (scaledninotime - prev_ninotime)/5e3;
+		    tpet_cycles = (scaledtpettime - prev_tpettime)/5e3;
+		    prev_tpettime = scaledtpettime;
+		    prev_ninotime = scaledninotime;
 
 		}
 //	    }
@@ -172,11 +199,16 @@ void synchplot()
     }
     TCanvas *c1 = new TCanvas("c1","c1",500,500);
     g1->SetTitle("H4DAQ time vs TOFPET trigger time; H4DAQ time; TOFPET time");
-    g1->SetMarkerStyle(7);
     g1->Draw("Ap");
-//    c1->SaveAs("H4DAQvsTOFPETtimes_allRuns_no_trigger_full_test.png");
-//    TCanvas *c2 = new TCanvas("c2","c2",500,500);
-//    g2->Draw("A*");
+//    c1->SaveAs("H4DAQvsTOFPETtimes_allRuns_no_trigger_zoom_beginning.png");
+    TCanvas *c2 = new TCanvas("c2","c2",500,500);
+    g2->SetTitle("TOFPET trigger time vs Time difference; TOFPET time (ps); Time difference(ps)");
+    g2->Draw("A*");
+    c2->SaveAs("TimeDifferenceASIC1.png");
+//    TCanvas *c3 = new TCanvas("c3","c3",500,500);
+//    g3->Draw("Ap");
+    
+
 }
 
 
